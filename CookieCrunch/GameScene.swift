60d962978a754321b07f131b80b55a7f1e7ca316
Kyle Olivo <kyleolivo@gmail.com>
2014-07-22 22:46:05 -0700

    Animate match points.

--- CookieCrunch/GameScene.swift
+++ CookieCrunch/GameScene.swift
@@ -44,6 +44,7 @@ class GameScene: SKScene {
         gameLayer.addChild(cookiesLayer)
         swipeFromColumn = nil
         swipeFromRow = nil
+        SKLabelNode(fontNamed: "GillSans-BoldItalic")
     }
     
     func hideSelectionIndicator() {
@@ -228,6 +229,7 @@ class GameScene: SKScene {
     
     func animateMatchedCookies(chains: Set<Chain>, completion: () -> ()) {
         for chain in chains {
+            animateScoreForChain(chain)
             for cookie in chain.cookies {
                 if let sprite = cookie.sprite {
                     if sprite.actionForKey("removing") == nil {
@@ -241,6 +243,27 @@ class GameScene: SKScene {
         }
         runAction(matchSound)
         runAction(SKAction.waitForDuration(0.3), completion: completion)
+    }
+    
+    func animateScoreForChain(chain: Chain) {
+        // Figure out what the midpoint of the chain is.
+        let firstSprite = chain.firstCookie().sprite!
+        let lastSprite = chain.lastCookie().sprite!
+        let centerPosition = CGPoint(
+            x: (firstSprite.position.x + lastSprite.position.x)/2,
+            y: (firstSprite.position.y + lastSprite.position.y)/2 - 8)
+        
+        // Add a label for the score that slowly floats up.
+        let scoreLabel = SKLabelNode(fontNamed: "GillSans-BoldItalic")
+        scoreLabel.fontSize = 16
+        scoreLabel.text = NSString(format: "%ld", chain.score)
+        scoreLabel.position = centerPosition
+        scoreLabel.zPosition = 300
+        cookiesLayer.addChild(scoreLabel)
+        
+        let moveAction = SKAction.moveBy(CGVector(dx: 0, dy: 3), duration: 0.7)
+        moveAction.timingMode = .EaseOut
+        scoreLabel.runAction(SKAction.sequence([moveAction, SKAction.removeFromParent()]))
     }
     
     func animateSwap(swap: Swap, completion: () -> ()) {
