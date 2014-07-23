37f6aaddc501168bb17e38ef2bb80a8948d14efa
Kyle Olivo <kyleolivo@gmail.com>
2014-07-22 23:07:55 -0700

    Make falling animations smoother.

--- CookieCrunch/GameScene.swift
+++ CookieCrunch/GameScene.swift
@@ -8,6 +8,9 @@ class GameScene: SKScene {
     let fallingCookieSound = SKAction.playSoundFileNamed("Scrape.wav", waitForCompletion: false)
     let addCookieSound = SKAction.playSoundFileNamed("Drip.wav", waitForCompletion: false)
     
+    let cropLayer = SKCropNode()
+    let maskLayer = SKNode()
+    
     var swipeHandler: ((Swap) -> ())?
 
     var level: Level!
@@ -42,7 +45,11 @@ class GameScene: SKScene {
         cookiesLayer.position = layerPosition
         tilesLayer.position = layerPosition
         gameLayer.addChild(tilesLayer)
-        gameLayer.addChild(cookiesLayer)
+        gameLayer.addChild(cropLayer)
+
+        maskLayer.position = layerPosition
+        cropLayer.maskNode = maskLayer
+        cropLayer.addChild(cookiesLayer)
         swipeFromColumn = nil
         swipeFromRow = nil
         SKLabelNode(fontNamed: "GillSans-BoldItalic")
@@ -333,8 +340,35 @@ class GameScene: SKScene {
         for row in 0..<NumRows {
             for column in 0..<NumColumns {
                 if let tile = level.tileAtColumn(column, row: row) {
-                    let tileNode = SKSpriteNode(imageNamed: "Tile")
+                    let tileNode = SKSpriteNode(imageNamed: "MaskTile")
                     tileNode.position = pointForColumn(column, row: row)
+                    maskLayer.addChild(tileNode)
+                }
+            }
+        }
+        for row in 0...NumRows {
+            for column in 0...NumColumns {
+                let topLeft     = (column > 0) && (row < NumRows)
+                    && level.tileAtColumn(column - 1, row: row)
+                let bottomLeft  = (column > 0) && (row > 0)
+                    && level.tileAtColumn(column - 1, row: row - 1)
+                let topRight    = (column < NumColumns) && (row < NumRows)
+                    && level.tileAtColumn(column, row: row)
+                let bottomRight = (column < NumColumns) && (row > 0)
+                    && level.tileAtColumn(column, row: row - 1)
+                
+                // The tiles are named from 0 to 15, according to the bitmask that is
+                // made by combining these four values.
+                let value = Int(topLeft) | Int(topRight) << 1 | Int(bottomLeft) << 2 | Int(bottomRight) << 3
+                
+                // Values 0 (no tiles), 6 and 9 (two opposite tiles) are not drawn.
+                if value != 0 && value != 6 && value != 9 {
+                    let name = String(format: "Tile_%ld", value)
+                    let tileNode = SKSpriteNode(imageNamed: name)
+                    var point = pointForColumn(column, row: row)
+                    point.x -= TileWidth/2
+                    point.y -= TileHeight/2
+                    tileNode.position = point
                     tilesLayer.addChild(tileNode)
                 }
             }
